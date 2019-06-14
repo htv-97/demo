df8d78ee1fca07e5de36a7ca38d5285d657eaeda
htv <vuth.gmc@gmail.com>
2019-06-15 03:14:56 +0700

    nothing to note

--- js/main.js
+++ js/main.js
@@ -1,52 +1,62 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var owl = $('.owl-carousel');
     var owl3 = $('.owl-carousel-3');
     var owl5 = $('.owl-carousel-5');
 
-   
 
-        owl.owlCarousel({
-            items: 1,
-            nav: true,
-            loop: true,
-            smartSpeed:400,
-            margin: 0,
-            autoplay: true,
-            autoplayTimeout: 1000,
-            autoplayHoverPause: true
-        });
-        owl3.owlCarousel({
-            center:true,
-            items: 3,
-            stagePadding:0,
-            smartSpeed:400,
-            loop: true,
-            margin: 20,
-            autoplay: true,
-            autoplayTimeout: 1000,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                  items: 1
-                },
-                600: {
-                  items: 3
-                }              
+
+    owl.owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        smartSpeed: 400,
+        margin: 0,
+        autoplay: false,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+    });
+    owl3.owlCarousel({
+        center: true,
+        items: 3,
+        stagePadding: 0,
+        smartSpeed: 400,
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
             }
-        });
-    
-        owl5.owlCarousel({
-            center:true,
-            smartSpeed:400,
-            items: 3,
-            stagePadding: 150,
-            nav:true,
-            loop: true,
-            margin: 20,
-            autoplay: false,
-            autoplayTimeout: 1000,
-            autoplayHoverPause: true
-        });
+        }
+    });
+
+    owl5.owlCarousel({
+        center: true,
+        smartSpeed: 400,
+        stagePadding: 150,
+        nav: true,
+        loop: true,
+        margin: 20,
+        autoplay: false,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            900: {
+                items: 5
+            }
+        }
+    });
 
 
 
@@ -63,5 +73,3 @@ $(document).ready(function () {
 
 
 })
-
-
